92a9c1f0184cbbdbae4d10c422294c22777a3991
Martin Ruggeri <martinruggeri.00@gmail.com>
2022-08-14 20:31:41 -0300

    update routes

--- Backend/src/server/routes.ts
+++ Backend/src/server/routes.ts
@@ -4,7 +4,7 @@ import * as express from "express";
 
 import * as authorization from "../authorization/auth.routes";
 import * as user from "../user/user.route";
-import * as rol from "../Rol/rol.route";
+import * as rol from "../rol/rol.route";
 
 /**
   * Desacoplamos las rutas

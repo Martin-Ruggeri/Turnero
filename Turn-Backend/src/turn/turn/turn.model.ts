5fd4fea552873563f3c3d8d9b37e016051e2859d
Martin Ruggeri <martinruggeri.00@gmail.com>
2022-08-24 20:44:41 -0300

    Update Model Turn

--- Turn-Backend/src/turn/turn/turn.model.ts
+++ Turn-Backend/src/turn/turn/turn.model.ts
@@ -1,17 +1,14 @@
 "use strict";
 
 import { Time } from "../../util/Time";
-import { ISchedule } from "../../schedule/schedule.model";
-import { IUser } from "../../user/user.model";
-import { IStateTurn } from "../stateTurn/stateTurn.model";
-
 
 export interface Iturn extends Document {
     turn_id : number;
     date: Date;
     start_time: Time;
     end_time: Time;
-    schedule: ISchedule;
-    user: IUser;
-    state: IStateTurn;
+    schedule_id: number;
+    user_id: number;
+    state_turn_code: string;
+    state_name: string;
 }
